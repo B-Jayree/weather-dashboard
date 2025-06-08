902bc3d0bb0ec5a3a1360ef1b22a2ef5ab3839d4
B-Jayree <benybwitonzi@gmail.com>
2025-06-08 09:45:55 +0200

    END OF Week 10 DAY 2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-
-import './STYLES/App.css';
+import React from 'react';
+import './STYLE/App.css';
 
 function App() {
   return (
     <div className="App">
+    <div>
+      <h2>New York</h2>
+      <p>Temperature: 15C</p>
+      <p>Condition: Sunny</p>
+    </div>
+     <div>
+      <h2>London</h2>
+      <p>Temperature: 10C</p>
+      <p>Condition: Rainy</p>
+    </div>
+         <div>
+      <h2>Tokyo</h2>
+      <p>Temperature: 20C</p>
+      <p>Condition: Cloudy</p>
+    </div>
     </div>
   );
 }
