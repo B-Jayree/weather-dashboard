5ac6243a6392e796caf23eb081f87207885e8dc0
B-Jayree <benybwitonzi@gmail.com>
2025-06-08 18:13:52 +0200

    End of Week 10

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import './styles/App.css';
 import WeatherDashboard from "./components/WeatherDashboard";
-// import { useState } from "react";
+import Home from './components/Home';
+
+
+import { useState } from "react";
 // import { LoginComponent } from './components/CONDITIONAL/LoginComponent';
 // import WelcomeComponent from './components/CONDITIONAL/WelcomeComponent';
 
 function App() {
-  // const [isLoggedIn, setIsLoggedIn] = useState(false);
-  return (
+  const [showWeatherDashboard, setShowWeatherDashboard] = useState(false);
+ return (
     <div className="App">
             <h1>Weather Dashboard</h1>
-            {/* <WeatherDashboard /> */}
-            {/* Conditional rendering */}
-            {/* {isLoggedIn ? <WelcomeComponent /> : <LoginComponent />}
-            <button style={{ padding: '10px', backgroundColor: 'blue', color: 'white', margin: '10px', border: 'none',  }} onClick={() => setIsLoggedIn(!isLoggedIn)}>
-              {isLoggedIn ? 'Logout' : 'Login'}
-
-            </button> */}
-            {/* End conditional Rendering */}
-            <WeatherDashboard />
+            {showWeatherDashboard ? (
+                <WeatherDashboard onBack={() => setShowWeatherDashboard(false)} />
+            ) : (
+                <Home onShowWeather={() => setShowWeatherDashboard(true)} />
+            )}
             
     </div>
   );
--- src/components/WeatherDashboard.jsx
+++ src/components/WeatherDashboard.jsx
@@ -2,13 +2,17 @@ import React from 'react'
 import CityWeather from './CityWeather'
 import '../styles/WeatherDashboard.css'
 
-export default function WeatherDashboard() {
+export default function WeatherDashboard({onBack}) {
   return (
     <div className='weather-dashboard'>
+       <button onClick={onBack} className='back-button'>Back</button>
+       <div className='cities'>
         <CityWeather city="New York" temperature="15C" condition="Sunny" />
         <CityWeather city="London" temperature="10C" condition="Rainy" />
         <CityWeather city="Tokyo" temperature="20C" condition="Cloudy" />
-        <div className='icon-placeholder'></div>
+        <CityWeather city="Sydney" temperature="25C" condition="Sunny" />
+        <CityWeather city="Moscow" temperature="-5C" condition="Snowy" />
+        </div>
     </div>
   )
 }
